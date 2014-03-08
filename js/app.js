d5b32c58faa93222d59db06a157b8f38362a633a
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2014-03-08 17:32:22 +0100

    Display time always in 'xx:xx' format

--- js/app.js
+++ js/app.js
@@ -37,38 +37,44 @@ function saveDurationsInCookies() {
   $.cookie('break-duration', $('input[name="break"]').val());
 }
 
+var timerInterval;
+var timeLeft;
+
+function startTimer(timer) {
+  clearInterval(timerInterval);
+  setTimeLeft(timer);
+  timerInterval = setInterval(function() {
+    timeLeft--;
+    minutes = Math.floor(timeLeft / 60);
+    seconds = timeLeft % 60;
+    if (minutes < 10) { minutes = '0' + minutes; }
+    if (seconds < 10) { seconds = '0' + seconds; }
+    $('#session').text(minutes + ':' + seconds);
+    if (timeLeft == 0) { stopTimer(timer); }
+  }, 1000);
+}
+
 var sounds = {
   session: new Audio("sounds/sessionEnd.ogg"),
   break: new Audio("sounds/breakEnd.ogg")
 };
-
 var flowStates = { session: 'break', break: 'session' }
-var timerInterval;
 
-function startTimer(timer) {
+function stopTimer(timer) {
   clearInterval(timerInterval);
-  $('#session').text($('input[name="'+timer+'"]').val() + ':0')
-  timerInterval = setInterval(function() {
-    sessionTime = $('#session').text().split(':');
-    minutes = sessionTime[0];
-    seconds = sessionTime[1];
+  sounds[timer].play();
 
-    if (seconds == '0') {
-      seconds = 59;
-      if (minutes != '0') { minutes--; }
-    } else {
-      seconds--;
-    }
+  if (runningFlow) { startTimer(flowStates[timer]); }
+}
 
-    $('#session').text(minutes + ':' + seconds);
-
-    if (seconds == 0 && minutes == 0) {
-      clearInterval(timerInterval);
-      sounds[timer].play();
-
-      if (runningFlow) { startTimer(flowStates[timer]); }
-    }
-  }, 1000);
+function setTimeLeft(timer) {
+  timerVal = $('input[name="'+timer+'"]').val()
+  if (timerVal.match(':')) {
+    _timerVal = timerVal.split(':')
+    timeLeft = _timerVal[0] * 60 + _timerVal[1];
+  } else {
+    timeLeft = timerVal * 60
+  }
 }
 
 var defaultDurations = { session: 25, break: 5 };
